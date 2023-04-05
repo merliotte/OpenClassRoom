c7bf7c397c0a5e44747bc4e022b09367339449ad
merliotte <livainugent@hotmail.fr>
2023-04-05 17:15:09 +0200

    dedededede dedededede

--- P5-Dev-Web-Kanap-master/front/html/js/script.js
+++ P5-Dev-Web-Kanap-master/front/html/js/script.js
@@ -6,7 +6,7 @@
         dataApi.then(async(responseData) => {
             const response = await responseData.json();
             console.table(response);
-        //dedede dedededededegi
+        //dedede dedededededegisz
         // .catch(err){
         // console.log(err);
     }
