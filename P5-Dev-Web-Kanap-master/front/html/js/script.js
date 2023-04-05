28b00430d38bf638cbf7752bb71bf6e93db24c01
merliotte <livainugent@hotmail.fr>
2023-04-05 17:13:05 +0200

    teste 3

--- P5-Dev-Web-Kanap-master/front/html/js/script.js
+++ P5-Dev-Web-Kanap-master/front/html/js/script.js
@@ -6,7 +6,7 @@
         dataApi.then(async(responseData) => {
             const response = await responseData.json();
             console.table(response);
-        //dedede dede
+        //dedede dededededede
         // .catch(err){
         // console.log(err);
     }
