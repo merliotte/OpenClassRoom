4bb26a2988e5f9331b54315aa15b05c9e0177ec5
merliotte <livainugent@hotmail.fr>
2023-04-09 09:30:50 +0200

    Amélioration SEO page 2

--- P5-Dev-Web-Kanap-master/front/js/product.js
+++ P5-Dev-Web-Kanap-master/front/js/product.js
@@ -5,21 +5,51 @@ const urlParams = new URL(document.location).searchParams;
 const urlId = urlParams.get("id");
 // Connexion avec l'API 
 const url = `http://localhost:3000/api/products/${urlId}`;
+const arrayId = [
+    {htmlElementId: "title",propsToLoadFromData:"name"}, 
+    {htmlElementId: "description", propsToLoadFromData:"description"},
+    {htmlElementId: "price", propsToLoadFromData:"price"},
+    {htmlElementId: ".item__img > img", propsToLoadFromData:"imageUrl", useMethodByClass: true},
+    {htmlElementId: ".item__img > img", propsToLoadFromData:"altTxt"},
+    
+];
 
 //  Création de la function et Récupération des informations de l'API avec fetch
- async function addElements() { 
-    await fetch(url)
+function addElements() { 
+     fetch(url)
     .then((response) => response.json())
     .then((data) => {
+        arrayId.forEach(addkanap => {
+            if(addkanap.useMethodByClass) {
+                document.querySelector(addkanap.htmlElementId).setAttribute("src",data[addkanap.propsToLoadFromData]) 
+                return
+            }
+            document.getElementById(addkanap.htmlElementId).innerHTML = data[addkanap.propsToLoadFromData]
+
+        })
+
+
+
+
+
+
+
 // Association des composants de la page HTML avec l'API
 console.log(data);
-const productName = document.getElementById("title").innerHTML= data.name;
-const productDescription = document.getElementById("description").innerHTML = data.name;
-const productPrice = document.getElementById("price").innerHTML=data.price; 
+
+//  document.getElementById("title").innerHTML= data.name;
+//  document.getElementById("description").innerHTML = data.description;
+//  document.getElementById("price").innerHTML= data.price; 
+//  document.getElementsByClassName(".item__img").innerHTML = data.imageUrl;
  });
-}
+ }
 // Appel à la fonction 
 addElements();
+
+
+
+
+
 
 
 
--- P5-Dev-Web-Kanap-master/front/js/script.js
+++ P5-Dev-Web-Kanap-master/front/js/script.js
@@ -1,43 +1,44 @@
-const url = " http://localhost:3000/api/products"; 
+const url = "http://localhost:3000/api/products"; 
 
  fetch(url)
-.then( async(response) => await response.json())
+.then( (response) => response.json())
 .then ((data) => {
 
-    for(let product in data) {
-        document.getElementById("items").innerHTML+= `
+    // for(let product in data) {
+        // document.getElementById("items").innerHTML+= `
     
-        <a href="./product.html?id=${data[product]._id}">
+        // // <a href="./product.html?id=${data[product]._id}">
+        // //     <article>
+        // //       <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
+        // //       <h3 class="productName">${data[product].name}</h3>
+        // //       <p class="productDescription">${data[product].description}</p>
+        // //     </article>
+        // //     `
+        //     window.location.href = `<a href="./product.html?id=${data[product]._id}">
+        //     <article>
+        //       <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
+        //       <h3 class="productName">${data[product].name}</h3>
+        //       <p class="productDescription">${data[product].description}</p>
+        //     </article>`
+        
+
+// .catch((error) => {
+//           console.log(error);
+//           window.alert("Désolé nous ne trouvons pas votre canapé.")
+//         });
+
+ // Méthode For Each
+data.forEach(kanap => { 
+  document.getElementById("items").innerHTML+= `
+    
+        <a href="./product.html?id=${kanap._id}">
             <article>
-              <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
-              <h3 class="productName">${data[product].name}</h3>
-              <p class="productDescription">${data[product].description}</p>
+              <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+              <h3 class="productName">${kanap.name}</h3>
+              <p class="productDescription">${kanap.description}</p>
             </article>
             `
-            window.location.href = `<a href="./product.html?id=${data[product]._id}">
-            <article>
-              <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
-              <h3 class="productName">${data[product].name}</h3>
-              <p class="productDescription">${data[product].description}</p>
-            </article>`
-        }
-        
-    
-})
-.catch((error) => {
-          console.log(error);
-          window.alert("Désolé nous ne trouvons pas votre canapé.")
-        });
-
-// const products = document.querySelectorAll('.product');
-
-// // Parcourir tous les éléments "product" et ajouter un événement "click" à chacun
-// products.forEach(product => {
-//   product.addEventListener('click', () => {
-//     // Récupérer l'identifiant de l'article sélectionné à partir de l'URL
-//     const productId = product.querySelector('a').href.split('?id=')[1];
-
-//     // Rediriger l'utilisateur vers la page de l'article en question
-//     window.location.href = `./product.html?id=${productId}`;
-//   });
-// });
+});
+}); 
+const array = [ "1", "2", "3"]
+console.log([1]);
