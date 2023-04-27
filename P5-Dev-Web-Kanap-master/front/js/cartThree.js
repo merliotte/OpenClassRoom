9c73b9072ffaf61e3ac805b99ff112ea75927921
merliotte <livainugent@hotmail.fr>
2023-04-27 12:10:36 +0200

    Ajout du bouton suppresion

--- P5-Dev-Web-Kanap-master/front/js/cartThree.js
+++ P5-Dev-Web-Kanap-master/front/js/cartThree.js
@@ -9,6 +9,12 @@ function createCartItemElement(data) {
   return fetch(`http://localhost:3000/api/products/${data.id}`)
     .then(response => response.json())
     .then(product => {
+        // Remplacement des articles 
+
+        if (cartItemsContainer){
+            const valueImg = document.querySelector(".cart__item");
+            valueImg.style.display = "none"; 
+        }
       // Création de l'élément article
       const articleElement = document.createElement("article");
       articleElement.classList.add("cart__item");
@@ -40,11 +46,18 @@ function createCartItemElement(data) {
       quantityElement.setAttribute("min", "1");
       quantityElement.setAttribute("max", "100");
       quantityElement.setAttribute("value", data.quantity);
+      quantityElement.style.width = "35px";
+      quantityElement.style.borderRadius = "15px";
+      quantityElement.style.textAlign = "center";
 
       // Création de l'élément p pour le bouton de suppression
-      const deleteButtonElement = document.createElement("p");
-      deleteButtonElement.classList.add("deleteItem");
-      deleteButtonElement.textContent = "Supprimer";
+      const deleteButtonElement = document.createElement("div");
+      deleteButtonElement.classList.add("cart__item__content__settings__delete");
+      const deleteItemElement = document.createElement("p");
+      deleteItemElement.classList.add("deleteItem");
+      deleteItemElement.textContent = "Supprimer";
+      deleteItemElement.style.cursor = "pointer"
+      deleteButtonElement.appendChild(deleteItemElement);
 
       // Ajout des éléments dans l'élément article
       const cartItemContentElement = document.createElement("div");
@@ -55,14 +68,36 @@ function createCartItemElement(data) {
       cartItemContentElement.appendChild(priceElement);
       cartItemContentElement.appendChild(quantityElement);
       cartItemContentElement.appendChild(deleteButtonElement);
+      cartItemContentElement.appendChild(deleteItemElement);
       articleElement.appendChild(cartItemContentElement);
 
       // Ajout de l'élément article au conteneur
       cartItemsContainer.appendChild(articleElement);
-    });
+    
+});
 }
 
 // Parcours des produits du panier et appel de la fonction createCartItemElement pour chacun d'eux
 if (cartItems) {
   cartItems.forEach(item => createCartItemElement(item));
 }
+
+ // Suppression des articles du Panier
+
+// Ajout d'un événement de clic au bouton de suppression
+function deleteElement (deleteButtonElement) {
+    deleteItem.addEventListener("click", () => {
+    // Supprimer l'élément article parent du bouton de suppression
+    const articleElement = deleteButtonElement.closest(".cart__item");
+    articleElement.parentNode.removeChild(articleElement);
+    // Vérifier si le conteneur est vide et afficher un message si c'est le cas
+    if (cartItemsContainer.children.length === 0) {
+      const emptyCartElement = document.createElement("p");
+      emptyCartElement.classList.add("empty-cart-message");
+      emptyCartElement.textContent = "Votre panier est vide";
+      cartItemsContainer.appendChild(emptyCartElement);
+    }
+  });
+}; 
+
+deleteElement(deleteButtonElement);
